5bc4dd735dbcdc2ae4e11d9e62a1f2d5c665c31e
Guisardo <missgatasch@gmail.com>
2026-03-07 03:45:08 -0300

    feat: fall back to lastSeen sort when recommendations are disabled
    When a user opts out of auto-adding recommendations to the watchlist,
    the watchlist default sort order switches from 'recommended' to
    'lastSeen' since no estimated ratings will be populated.

--- client/src/pages/WatchlistPage.tsx
+++ client/src/pages/WatchlistPage.tsx
@@ -1,11 +1,16 @@
 import React, { FunctionComponent } from 'react';
 import { PaginatedGridItems } from 'src/components/PaginatedGridItems';
+import { useUser } from 'src/api/user';
 
 export const WatchlistPage: FunctionComponent = () => {
+  const { user } = useUser();
+  const orderBy =
+    user.addRecommendedToWatchlist !== false ? 'recommended' : 'lastSeen';
+
   return (
     <PaginatedGridItems
       args={{
-        orderBy: 'recommended',
+        orderBy,
         sortOrder: 'desc',
         onlyOnWatchlist: true,
       }}
